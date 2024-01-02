4e4ecd2c4986e11c1fef1797cebfb135696fc83f
Katakras <katakrask@gmail.com>
2024-01-02 23:59:21 +0000

    added boost testing to project

--- cpp/test/main.cpp
+++ cpp/test/main.cpp
@@ -1,11 +1,14 @@
-#include <iostream>
+#define BOOST_TEST_MAIN
+#define BOOST_TEST_DYN_LINK
+
 #include <boost/test/unit_test.hpp>
 
 #include "matrix.hpp"
 
-int main()
-{
-    std::cout << "Hello world!" << std::endl;
-    auto matrix = std::make_shared<Matrix>(1u, 1u);
 
+BOOST_AUTO_TEST_CASE(matrix_test)
+{
+    auto matrix = std::make_shared<Matrix>(1u, 1u);
+    BOOST_CHECK_EQUAL(matrix->n_cols(), 1u);
+    BOOST_CHECK_EQUAL(matrix->n_rows(), 1u);
 }
