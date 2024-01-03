8f8fbc2ff7c5f5e830e468a6561d423dc35b4b17
Katakras <katakrask@gmail.com>
2024-01-03 22:01:17 +0000

    improved test structures with CTest and Boost

---
+++ cpp/test/matrix_test.cpp
@@ -0,0 +1,23 @@
+#include <boost/test/unit_test.hpp>
+
+
+#include "matrix.hpp"
+
+BOOST_AUTO_TEST_SUITE(matrices)
+
+BOOST_AUTO_TEST_CASE(matrix_creation)
+{
+    auto matrix = std::make_shared<Matrix>(1u, 1u);
+    BOOST_CHECK_EQUAL(matrix->n_cols(), 1u);
+    BOOST_CHECK_EQUAL(matrix->n_rows(), 1u);
+}
+
+BOOST_AUTO_TEST_CASE(matrix_resizing)
+{
+    auto matrix = std::make_shared<Matrix>(1u, 1u);
+    matrix->resize(2u, 2u);
+    BOOST_CHECK_EQUAL(matrix->n_cols(), 2u);
+    BOOST_CHECK_EQUAL(matrix->n_rows(), 2u);
+}
+
+BOOST_AUTO_TEST_SUITE_END()
